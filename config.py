b0cba63fcadc55f20e3a370ba76d05d66d322144
yfree1232-afk <yfree1232@gmail.com>
2026-03-28 21:20:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
+api_id = "1888747"
+api_hash = "0d707e8ae15254b1453c614bf3026c32"
+bot_token = "8755124701:AAEtlT5aHCE6mPOvJWF_JTxfMGRhu1Jod_w"
+auth_users = [8342248523]
 sudo_users = [7890781002]
